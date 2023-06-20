4fcdc16c6f84aa736591a5260b103bb66175b825
takaken0518 <twiceup0518@gmail.com>
2023-06-20 10:12:49 +0900

    ラジオボタン追加作成中

---
+++ src/YMG.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './radiobutton'
+
+function YMG() {
+  return (
+    <div className="YMG">
+
+    </div>
+  );
+}
+
+export default YMG;
---
+++ src/radiobutton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const radiobutton = () => {
+  return (
+    <div>radiobutton</div>
+  )
+}
+
+export default radiobutton;
