6e270f5ae340ac397c663a181be5a3cdea0d39c5
takaken0518 <twiceup0518@gmail.com>
2023-07-18 12:02:19 +0900

    モーダルウィンドウ上で投票を行えるように修正

---
+++ src/ModalWindow.js
@@ -0,0 +1,52 @@
+import './modal.css'
+
+const ModalWindow = ({ selectedImage, votes, handleVote, handleCloseModal }) => {
+  console.log("モーダルウィンドウ１表示中");
+
+  const handleVoteButtonClick = () => {
+    handleVote();
+    handleCloseModal();
+  };
+
+  if(selectedImage === 'image1'){
+    return (
+    
+      <div>
+        <div id="overlay">
+          <div id="content">
+            <p>
+              <button className='closebutton' onClick={handleCloseModal}>close</button>
+            </p>
+            <p><img src='images/Cook.png' className='cook_pasta'></img></p>
+            <p>Votes: {votes[selectedImage]}</p>
+            <button className='VoteComplete' onClick={handleVoteButtonClick}>Vote</button>
+  
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
+  if(selectedImage === 'image2'){
+    return (
+    
+      <div>
+        <div id="overlay">
+          <div id="content">
+            <p>
+              <button className='closebutton' onClick={handleCloseModal}>close</button>
+            </p>
+            <p><img src='images/Yummy.png' className='yummy_pasta'></img></p>
+            <p>Votes: {votes[selectedImage]}</p>
+            <button className='VoteComplete' onClick={handleVoteButtonClick}>Vote</button>
+  
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+};
+
+export default ModalWindow;
--- src/VoteSystem.js
+++ src/VoteSystem.js
@@ -1,64 +1,66 @@
 import React, { useState } from 'react';
-import  './imagesAnime.css'
-
+import './imagesAnime.css';
+import ModalWindow from './ModalWindow';
 
 function VoteSystem() {
-    const [votes, setVotes] = useState({ image1: 0, image2: 0 });
-    const [selectedImage, setSelectedImage] = useState(null);
-    const [hasVoted, setHasVoted] = useState(false);
-  
-    const handleVote = () => {
-      if (selectedImage && !hasVoted) {
-        setVotes((prevVotes) => ({
-          ...prevVotes,
-          [selectedImage]: prevVotes[selectedImage] + 1,
-        }));
-        setHasVoted(true);
-      }
-    };
-  
-    const handleImageSelect = (image) => {
-      if (!hasVoted) {
-        setSelectedImage(image);
-      }
-    };
-  
-    return (
-      <div>
+  const [votes, setVotes] = useState({ image1: 0, image2: 0 });
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [hasVoted, setHasVoted] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-        <div>
-          <div
-            className={`Votebutton Cook ${selectedImage === 'image1' ? 'selected' : ''}`}
-            onClick={() => handleImageSelect('image1')}
-          >
-            <img src="images/Cook.png" alt="Image 1" className="vote-image Cook" />
-            {selectedImage === 'image1' && <div className="frame-animation"></div>}
-          </div>
-          {console.log("イケザキ投票数" + votes.image1)}
-        </div>
-        <div>
-          <div
-            className={`Votebutton Yummy ${selectedImage === 'image2' ? 'selected' : ''}`}
-            onClick={() => handleImageSelect('image2')}
-          >
-            <img src="images/Yummy.png" alt="Image 2" className="vote-image Yummy" />
-            {selectedImage === 'image2' && <div className="frame-animation"></div>}
-          </div>
+  const handleVote = () => {
+    if (selectedImage && !hasVoted) {
+      setVotes((prevVotes) => ({
+        ...prevVotes,
+        [selectedImage]: prevVotes[selectedImage] + 1,
+      }));
+      setHasVoted(true);
+    }
+  };
 
-          {console.log("Yummy投票数" + votes.image2)}
+  const handleImageSelect = (image) => {
+    if (!hasVoted) {
+      setSelectedImage(image);
+      setShowModal(true);
+    }
+  };
 
-        </div>
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+    setShowModal(false);
+  };
 
+  return (
+    <div>
+      <img
+        src="images/Cook.png"
+        alt="Image 1"
+        className="vote-image Cook"
+        onClick={() => handleImageSelect('image1')}
+      />
+      {console.log('イケザキ投票数' + votes.image1)}
 
-        <button class="VoteComplete" onClick={handleVote} disabled={!selectedImage || hasVoted}>
+      <img
+        src="images/Yummy.png"
+        alt="Image 2"
+        className="vote-image Yummy"
+        onClick={() => handleImageSelect('image2')}
+      />
+      {console.log('Yummy投票数' + votes.image2)}
 
-        <span>
-        投票する!!
-        </span>
+      <p></p>
 
-        </button>
-      </div>
-    );
-  }
-  
-  export default VoteSystem;
+      {showModal && (
+        <ModalWindow
+        selectedImage={selectedImage}
+        votes={votes}
+        handleVote={handleVote}
+        handleCloseModal={handleCloseModal}
+          />
+      )}
+
+    </div>
+  );
+}
+
+export default VoteSystem;
